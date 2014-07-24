0e6846f1a9df81d9dd1dbb887349b9780f36e53e
jamieborne2 <jamie.borne2@gmail.com>
2014-07-24 23:16:45 +0000

    update

--- master/my-lession.js
+++ master/my-lession.js
@@ -10,32 +10,8 @@ var person = {};
 console.log(person);
 
 // TODO 3 : Add firstName and lastName properties to our person:
-person.firstName = "user";
-person.lastName = "#1";
+person.firstName = "_____";
+person.lastName = "_____";
 console.log("First Name: " + person.firstName);
 console.log("Last Name: " + person.lastName);
-
-// TODO 4 : Add the city property using Array syntax:
-person.city = "New orleans";
-console.log("city: " + person.city);
-
-// TODO 5 : Create a function that prints the firstName, lastName and city of the person:
-person.sayHello = function() {
-  console.log("Hello, my name is " + this.firstName + " " +
-this.lastName + ", I live in " + this.city);
-};
-person.sayHello();
-
-// TODO 6 : Create an Array of likes:
-person.likes = ["music", "food", "stuff", "pizza", "pets", "project space"];
-
-// TODO 7 : Create a function on person that prints all of the things the person likes:
-person.sayLikes = function() {
-  console.log("I like: ");
-  for (index in this.likes) {
-    console.log("\t" + this.likes[index]);
-  }
-};
-person.sayLikes();
-
 
