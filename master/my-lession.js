961bb5a7317fd34caa27bd233cb51c91a2a0f20c
jamieborne2 <jamie.borne2@gmail.com>
2014-07-24 03:16:54 +0000

    update

--- master/my-lession.js
+++ master/my-lession.js
@@ -1,19 +1,22 @@
 #!/usr/bin/env node
 
 // TODO 1 : Create a variable called welcomeMessage and assign it a string
-
+var welcomeMessege = "Welcome to your awsome node app!";
+console.log(welcomeMessege);
 
 
 // TODO 2 : Create a variable called person of type Object using the Object literal syntax:
-
-
+var person = {};
+console.log(person);
 
 // TODO 3 : Add firstName and lastName properties to our person:
-
-
+person.firstName = "user";
+person.lastName = "#1";
+console.log("First Name: " + person.firstName);
+console.log("Last Name: " + person.lastName);
 
 // TODO 4 : Add the city property using Array syntax:
-person.city = "New Orleans";
+person.city = "New orleans";
 console.log("city: " + person.city);
 
 // TODO 5 : Create a function that prints the firstName, lastName and city of the person:
@@ -34,4 +37,5 @@ person.sayLikes = function() {
   }
 };
 person.sayLikes();
+
 
